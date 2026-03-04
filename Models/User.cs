7fc0d726469f10b661facf8deb160565c1194d8b
ste <mrbarrow@hotmail.co.uk>
2026-03-04 14:27:19 +0000

    feat:User - database test

---
+++ Models/User.cs
@@ -0,0 +1,9 @@
+namespace task_tracker.Models
+{
+    public class User
+    {
+        public int Id {get; set; } //Primary Key
+        public string Name {get; set; }
+        public string Email {get; set; }
+    }
+}
