6f6281e198e7de63babd0137c10f37b491049843
ste <mrbarrow@hotmail.co.uk>
2026-03-04 08:50:57 +0000

    initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+namespace task_tracker;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        var app = builder.Build();
+
+        app.MapGet("/", () => "Hello World!");
+
+        app.Run();
+    }
+}
